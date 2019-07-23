3c9a52adb4866152df541504b6f34b356f82c988
Felix Hedenström <fhede@kth.se>
2019-07-23 19:26:59 +0200

    Added basic BFS

--- src/graph.rs
+++ src/graph.rs
@@ -1,22 +1,67 @@
 use std::collections::HashMap;
-use std::collections::LinkedList;
+use std::collections::VecDeque;
+use std::collections::HashSet;
 
 
-struct Graph<T> {
-    neighbors: HashMap<T, LinkedList<T>>
+pub struct Graph<T: std::cmp::Eq + std::hash::Hash + std::marker::Copy + std::fmt::Debug> {
+    pub neighbors: HashMap<T, Vec<T>>,
+    pub nodes: Vec<T>
 }
 
+impl <T: std::cmp::Eq + std::hash::Hash + std::marker::Copy + std::fmt::Debug> Graph<T>{
+
+    fn get_neighbors(&self, node: &T) -> std::option::Option<&Vec<T>>{
+        return self.neighbors.get(node);
+    }
 
 
+    pub fn bfs(&self, start: &T, end: &T) -> std::option::Option<Vec<T>> {
+        let mut queue: VecDeque<&T> = VecDeque::new();
+        let mut node: &T = start; 
+        let mut parent: HashMap<&T,&T> = HashMap::new();
+        let mut visited: HashSet<&T> = HashSet::new();
 
-pub fn bfs() -> i32 {
-    let mut h = HashMap::new();
 
-    h.insert(1, LinkedList::new());
+        queue.push_back(node);
+        visited.insert(node);
 
-    let g = Graph{neighbors: h}; 
+        while !queue.is_empty() {
+            node = queue.pop_front().expect("node value was not found");   
+            
+            if node == end {
+                break;
+            }
 
-    return g.neighbors.get(&1).size();
+            for neighbor in self.get_neighbors(node).expect("node did not have neighbors"){
+                if !visited.contains(neighbor){
+                    visited.insert(neighbor);
+                    parent.insert(neighbor, node);
+                
+                    queue.push_back(neighbor);
+                }
+            }
 
+        }
+
+        node = end;
+        
+        if !parent.contains_key(&end){
+            return None;
+        }
+       
+        let mut path: Vec<T> = Vec::new();
+        path.push(
+            node.clone()
+        );
+        
+        while parent.contains_key(node){
+            node = parent.get(node).expect("did not recieve node when expected");
+            path.push(
+                node.clone()
+            );
+        }
+
+        return Some(path);
+    
+    }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod graph;
 
+use std::collections::HashMap;
 
 fn main() {
-    println!("{}", graph::bfs());
+    let mut h: HashMap<i32, Vec<i32>> = HashMap::new();
+
+    h.insert(1, vec![2]);
+    h.insert(2, vec![1,3]);
+    h.insert(3, vec![1,2,3,5]);
+    h.insert(4, vec![1]);
+    h.insert(5, vec![4]);
+
+    let g = graph::Graph::<i32>{
+            neighbors: h,
+            nodes: vec![1,2,3,4,5]
+    };
+
+    println!("{:?}", g.bfs(&1,&4))
 }
