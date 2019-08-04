940365cac3c29cdda1731d81bbfbb758d31ceab6
Felix Hedenström <fhede@kth.se>
2019-08-04 11:51:45 +0200

    Improved some python->rust code and added a super basic python test file

---
+++ python_test.py
@@ -0,0 +1,67 @@
+import target.debug.libngramconnector as lnc
+import pprint
+
+example_ngram = {
+        "this":
+            {
+                "is":
+                    {
+                        "a": 1,
+                        "one": 2,
+                        "an": 3
+                    },
+                "should":
+                    {
+                        "work": 1
+                    }
+            },
+        "is":
+            {
+                "an":
+                    {
+                        "example": 1
+                    }
+            },
+        "an":
+            {
+                "example":
+                    {
+                        "of": 1    
+                    }
+            },
+        "example":
+            {
+                "of":
+                    {
+                        "a": 1
+                    }
+            },
+        "of":
+            {
+                "a":
+                    {
+                        "3gram": 1    
+                    }
+            },       
+        "a":
+            {
+                "3gram":
+                    {
+                        "data": 1
+                    }
+            },
+        "3gram":
+            {
+                "data":
+                    {
+                        "structure": 1
+                    }
+            }
+    }
+
+pprint.pprint(example_ngram) 
+print(lnc.depth({"a": 1}))
+print(lnc.depth({"a": {"b": 1}}))
+print(lnc.depth(example_ngram))
+print(lnc.bfs(example_ngram, 3))
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 mod graph;
-
 #[macro_use]
 extern crate cpython;
 
-
-use cpython::{Python, PyResult, PyDict};
+use cpython::{Python, PyResult, PyDict, PyList, PyInt, PyErr};
 
 fn test(_py: Python, val: PyDict) -> PyResult<u32> {
     return Ok(1);
@@ -12,18 +10,27 @@ fn test(_py: Python, val: PyDict) -> PyResult<u32> {
 
 
 fn depth(_py: Python, val: PyDict) -> PyResult<u32> {
-    return Ok(depth_interal(_py, val));
+    return Ok(depth_internal(_py, val));
+}
+
+fn bfs(_py: Python, val: PyDict, n: PyInt) -> PyResult<PyInt> {
+    return Ok(n);
 }
 
 
-fn depth_interal(_py: Python, val: PyDict) -> u32 {
+/// Temporary code to test how to interface with PyDict 
+fn depth_internal(_py: Python, val: PyDict) -> u32 {
     if val.len(_py) == 0{
-        return 0
+        return 1;
     }
-    for i in val.items(_py){
-        return 0;
+    for (_k,v) in val.items(_py){
+        let potential_dict: PyResult<PyDict> = v.extract::<PyDict>(_py);
+        match potential_dict {
+            Ok(v) => return 1 + depth_internal(_py, v),
+            Err(_e) => return 1,
+        }
     }
-    return 0;
+    return 1;
 
 }
 
@@ -33,5 +40,6 @@ py_module_initializer!(libngramconnector, initlibngramconnector, PyInit_libngram
     r#try!(m.add(py, "__doc__", "This module is implemented in Rust"));
     r#try!(m.add(py, "test", py_fn!(py, test(val: PyDict))));
     r#try!(m.add(py, "depth", py_fn!(py, depth(val: PyDict))));
+    r#try!(m.add(py, "bfs", py_fn!(py, bfs(val: PyDict, n: PyInt))));
     Ok(())
 });
